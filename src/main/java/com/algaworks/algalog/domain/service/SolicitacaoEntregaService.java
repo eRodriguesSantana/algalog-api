c70b04d0bc08b6661cacc648329e15f3fa100a4b
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2022-11-07 20:05:28 -0300

    Adicionado metodos listar() e buscaPorId()

--- src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -1,10 +1,12 @@
 package com.algaworks.algalog.domain.service;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algalog.domain.exception.NegocioException;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.model.StatusEntrega;
@@ -30,4 +32,14 @@ public class SolicitacaoEntregaService {
 		return entregaRepository.save(entrega);
 	}
 	
+	@Transactional
+	public List<Entrega> listar(){
+		return entregaRepository.findAll();
+	}
+	
+	@Transactional
+	public Entrega buscaPorId(Long entregaId) {
+		return entregaRepository.findById(entregaId)
+				.orElseThrow(() -> new NegocioException("Entrega não encontrada"));
+	}
 }
