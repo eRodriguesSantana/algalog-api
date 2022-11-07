abf75f3b000e77af663c7f5a694971c374a687fe
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2022-11-07 02:25:29 -0300

    Adicionado dependencia flyway e feito primeira consulta no banco de dados atraves do EntityManager

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,12 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("Eduardo");
-		c1.setTelefone("12 111122222");
-		c1.setEmail("edu@edu.com");
-		
-		Cliente c2 = new Cliente();
-		c2.setId(2L);
-		c2.setNome("Eduardo2");
-		c2.setTelefone("12 111122222");
-		c2.setEmail("edu2@edu.com");
-		
-		return Arrays.asList(c1, c2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
