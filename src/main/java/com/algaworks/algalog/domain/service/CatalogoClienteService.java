0cdb2da74b7df3e0404d205b0c46b9809eeabd18
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2022-11-07 14:24:33 -0300

    Finalizacao da implementacao do Domain Services

--- src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algalog.domain.exception.NegocioException;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
@@ -19,6 +20,12 @@ public class CatalogoClienteService {
 	
 	@Transactional
 	public Cliente salvar(Cliente cliente) {
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+				.stream()
+				.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+		
+		if(emailEmUso)
+			throw new NegocioException("Já existe um cliente cadastrado com esse email");
 		return clienteRepository.save(cliente);
 	}
 	
