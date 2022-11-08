6f5b80b8154522abbbfa689584ce42e9c5a46950
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2022-11-07 22:41:17 -0300

    Implementando sub-recursos

--- src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,23 +17,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.algaworks.algalog.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algalog.domain.exception.NegocioException;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @ControllerAdvice
-public class APiExceptionHandler extends ResponseEntityExceptionHandler{
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private MessageSource messageSource;
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
 		List<Problema.Campo> campos = new ArrayList<>();
 		
-		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
 			String nome = ((FieldError) error).getField();
 			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 			
@@ -46,18 +46,31 @@ public class APiExceptionHandler extends ResponseEntityExceptionHandler{
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
 		problema.setCampos(campos);
 		
-		return super.handleExceptionInternal(ex, problema, headers, status, request);
+		return handleExceptionInternal(ex, problema, headers, status, request);
 	}
 	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request){
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<Object> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setDataHora(OffsetDateTime.now());
 		problema.setTitulo(ex.getMessage());
-
+		
 		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
 	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(OffsetDateTime.now());
+		problema.setTitulo(ex.getMessage());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
 }
